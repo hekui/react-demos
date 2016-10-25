763df9829af4266b5f14b98f4641d561d054b03e
hekui <360967863@qq.com>
2016-10-25 13:40:47 +0800

    添加helloworld将jsx编译为js的版本

---
+++ dest/.module-cache/139fc35bde59502c26dd3b542b6532270a6cf5a8.js
@@ -0,0 +1,27 @@
+ReactDOM.render(
+	React.createElement("h1", null, "hello world"),
+	document.getElementById("root")
+)
+//root#2
+var names=['jack','rose'];
+ReactDOM.render(
+	React.createElement("div", null, 
+		React.createElement("div", null, "遍历数组数据："), 
+		names.map(function(name){
+			return React.createElement("div", null, "hello,", name, "!")
+		})
+	),
+	document.getElementById("root2")
+)
+//root#3
+var arr=[
+	React.createElement("div", null, "hello jack"),
+	React.createElement("div", null, "hello rose")
+]
+ReactDOM.render(
+	React.createElement("div", null, 
+		React.createElement("div", null, "直接引用数组数据："), 
+		arr
+	),
+	document.getElementById("root3")
+)
---
+++ src/helloworld.js
@@ -0,0 +1,27 @@
+ReactDOM.render(
+	<h1>hello world</h1>,
+	document.getElementById("root")
+)
+//root#2
+var names=['jack','rose'];
+ReactDOM.render(
+	<div>
+		<div>遍历数组数据：</div>
+		{names.map(function(name){
+			return <div>hello,{name}!</div>
+		})}
+	</div>,
+	document.getElementById("root2")
+)
+//root#3
+var arr=[
+	<div>hello jack</div>,
+	<div>hello rose</div>
+]
+ReactDOM.render(
+	<div>
+		<div>直接引用数组数据：</div>
+		{arr}
+	</div>,
+	document.getElementById("root3")
+)
